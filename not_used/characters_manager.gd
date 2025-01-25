28273e882e3fe550c7ee7f905f7fd53fc85f7e16
Ainsley <vhtmscyo@gmail.com>
2025-01-25 14:40:57 +0100

    remove unnecessary staff

--- not_used/characters_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-class_name CharacterManager
-
-var selected: CharacterClick
-
-func _on_charcater_picked(character: CharacterClick) -> void:
-	if selected:
-		selected.drop()
-	selected = character
--- not_used/click_area.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-signal clicked
-
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.is_action_pressed("pick"):
-			clicked.emit()
