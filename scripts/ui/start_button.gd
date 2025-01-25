6b24987684657cfddca38d5ee0aab195ab6b9937
Ainsley <vhtmscyo@gmail.com>
2025-01-25 11:14:19 +0100

    A simple in game menu

--- scripts/ui/start_button.gd
+++ scripts/ui/start_button.gd
@@ -3,4 +3,6 @@ extends Button
 @export var first_level: PackedScene
 
 func _on_pressed() -> void:
+	print("change scene to ", first_level.resource_name)
 	get_tree().change_scene_to_packed(first_level)
+	
